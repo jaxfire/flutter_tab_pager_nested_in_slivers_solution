cf9c12393d01e0189ac1d8e930c30dfe0231d5a7
jamesashdepop <81640222+jamesashdepop@users.noreply.github.com>
2021-10-07 17:45:14 +0100

    Demo ready

--- lib/main.dart
+++ lib/main.dart
@@ -8,68 +8,16 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Kindacode.com',
-      theme: ThemeData(
-          primaryColor: Colors.purple, accentColor: Colors.yellowAccent),
+    return const MaterialApp(
       home: HomePage(),
     );
   }
 }
 
-const List<String> data = [
-  'Zeroth',
-  'First',
-];
-
 const List<Tab> tabs = <Tab>[
-  Tab(text: 'Zeroth'),
-  Tab(text: 'First'),
+  Tab(text: 'Tab 1'),
+  Tab(text: 'Tab 2'),
 ];
-
-// /// This is the stateless widget that the main application instantiates.
-// class MyStatelessWidget extends StatefulWidget {
-//   const MyStatelessWidget({Key? key}) : super(key: key);
-//
-//   @override
-//   State<MyStatelessWidget> createState() => _MyStatelessWidgetState();
-// }
-//
-// class _MyStatelessWidgetState extends State<MyStatelessWidget> {
-//   int selectedTab = 0;
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return DefaultTabController(
-//       length: tabs.length,
-//       // The Builder widget is used to have a different BuildContext to access
-//       // closest DefaultTabController.
-//       child: Builder(
-//         builder: (BuildContext context) {
-//           final TabController tabController = DefaultTabController.of(context)!;
-//           tabController.addListener(() {
-//             if (!tabController.indexIsChanging) {
-//               setState(() {
-//                 selectedTab = tabController.index;
-//               });
-//             }
-//           });
-//           return Scaffold(
-//             appBar: AppBar(
-//               bottom: const TabBar(
-//                 tabs: tabs,
-//               ),
-//             ),
-//             body: Center(
-//               child: Text('stuff $selectedTab'),
-//             ),
-//           );
-//         },
-//       ),
-//     );
-//   }
-// }
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -85,8 +33,6 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: tabs.length,
-      // The Builder widget is used to have a different BuildContext to access
-      // closest DefaultTabController.
       child: Builder(
         builder: (BuildContext context) {
           final TabController tabController = DefaultTabController.of(context)!;
@@ -100,35 +46,44 @@ class _HomePageState extends State<HomePage> {
           return Scaffold(
             body: CustomScrollView(
               slivers: <Widget>[
-                SliverToBoxAdapter(
-                  child: Container(
-                    color: Colors.pink,
-                    height: 400.0,
-                  ),
-                ),
+                AnyOldSliverWidget(colour: Colors.tealAccent),
+                AnyOldSliverWidget(colour: Colors.blue),
+                AnyOldSliverWidget(colour: Colors.green),
                 SliverToBoxAdapter(
                   child: TabBar(
                     labelColor: Colors.black,
                     indicatorColor: Colors.black,
-                    tabs: [
-                      Tab(text: 'Tab 1'),
-                      Tab(text: 'Tab 2'),
-                    ],
+                    tabs: tabs,
                   ),
                 ),
                 SliverToBoxAdapter(
                   child: MyTabBarView(selectedTab: selectedTab),
                 ),
-                SliverToBoxAdapter(
-                  child: Container(
-                    color: Colors.pink,
-                    height: 400.0,
-                  ),
-                ),
+                AnyOldSliverWidget(colour: Colors.red),
+                AnyOldSliverWidget(colour: Colors.orange),
+                AnyOldSliverWidget(colour: Colors.blue),
               ],
             ),
           );
         },
+      ),
+    );
+  }
+}
+
+class AnyOldSliverWidget extends StatelessWidget {
+  final Color colour;
+  const AnyOldSliverWidget({Key? key, required this.colour}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverToBoxAdapter(
+      child: Container(
+        color: colour,
+        height: 150.0,
+        child: const Center(
+          child: Text('I AM A SLIVER'),
+        ),
       ),
     );
   }
@@ -141,26 +96,12 @@ class MyTabBarView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> theWidgets = [MyThing()];
-    if (selectedTab == 1) {
-      theWidgets.add(MyThing());
-    }
-    return Column(
-      children: theWidgets,
-    );
-  }
-}
-
-class MyThing extends StatelessWidget {
-  const MyThing({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
     return Container(
-      width: double.infinity,
       color: Colors.yellow,
-      height: 50.0,
-      child: Text('Text 1'),
+      height: (selectedTab == 0) ? 100.0 : 200.0,
+      child: const Center(
+        child: Text('MY HEIGHT IS DYNAMIC'),
+      ),
     );
   }
 }
