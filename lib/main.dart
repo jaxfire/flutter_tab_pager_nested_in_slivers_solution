1e1d6b51be80e6dab299a0ab213fd6fc9dd5020a
jamesashdepop <81640222+jamesashdepop@users.noreply.github.com>
2021-10-07 16:52:10 +0100

    Simplified version

--- lib/main.dart
+++ lib/main.dart
@@ -18,123 +18,94 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SilverAppBarWithTabBarScreen(),
-    );
-  }
-}
-
-class SilverAppBarWithTabBarScreen extends StatefulWidget {
-  const SilverAppBarWithTabBarScreen({Key? key}) : super(key: key);
-
-  @override
-  _SilverAppBarWithTabBarState createState() => _SilverAppBarWithTabBarState();
-}
-
-class _SilverAppBarWithTabBarState extends State<SilverAppBarWithTabBarScreen>
-    with SingleTickerProviderStateMixin {
-  TabController? controller;
-
-  @override
-  void initState() {
-    super.initState();
-    controller = TabController(length: 3, vsync: this);
-  }
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: <Widget>[
-          SliverToBoxAdapter(
-            child: Container(
-              color: Colors.pink,
-              height: 400.0,
+      body: DefaultTabController(
+        // The number of tabs / content sections to display.
+        length: 3,
+        child: CustomScrollView(
+          slivers: <Widget>[
+            SliverToBoxAdapter(
+              child: Container(
+                color: Colors.pink,
+                height: 400.0,
+              ),
             ),
-          ),
-          SliverAppBar(
-            toolbarHeight: 0.0,
-            collapsedHeight: 0.0,
-            expandedHeight: 0.0,
-            bottom: TabBar(
-              tabs: [
-                Tab(text: 'Tab 1'),
-                Tab(text: 'Tab 2'),
-                Tab(text: 'Tab 3'),
-              ],
-              controller: controller,
+            SliverToBoxAdapter(
+              child: TabBar(
+                labelColor: Colors.black,
+                tabs: [
+                  Tab(text: 'Tab 1'),
+                  Tab(text: 'Tab 2'),
+                  Tab(text: 'Tab 3'),
+                ],
+              ),
             ),
-          ),
-          SliverFillRemaining(
-            child: SizedBox(
-              height: 400.0,
-              child: TabBarView(
-                controller: controller,
-                children: [
-                  Column(children: [
-                    Container(
-                      width: double.infinity,
-                      color: Colors.yellow,
-                      height: 50.0,
-                      child: Text('Text 1'),
-                    ),
-                  ]),
-                  Column(children: [
-                    Container(
-                      width: double.infinity,
-                      color: Colors.yellow,
-                      height: 50.0,
-                      child: Text('Text 2'),
-                    ),
-                    Container(
-                      width: double.infinity,
-                      color: Colors.green,
-                      height: 50.0,
-                      child: Text('Text 2'),
-                    ),
-                  ]),
-                  Column(
-                    children: [
+            SliverToBoxAdapter(
+              child: SizedBox(
+                height: 400.0,
+                child: TabBarView(
+                  children: [
+                    Column(children: [
                       Container(
                         width: double.infinity,
                         color: Colors.yellow,
                         height: 50.0,
-                        child: Text('Text 3'),
+                        child: Text('Text 1'),
+                      ),
+                    ]),
+                    Column(children: [
+                      Container(
+                        width: double.infinity,
+                        color: Colors.yellow,
+                        height: 50.0,
+                        child: Text('Text 2'),
                       ),
                       Container(
                         width: double.infinity,
                         color: Colors.green,
                         height: 50.0,
-                        child: Text('Text 3'),
+                        child: Text('Text 2'),
                       ),
-                      Container(
-                        width: double.infinity,
-                        color: Colors.red,
-                        height: 50.0,
-                        child: Text('Text 3'),
-                      ),
-                    ],
-                  ),
-                ],
+                    ]),
+                    Column(
+                      children: [
+                        Container(
+                          width: double.infinity,
+                          color: Colors.yellow,
+                          height: 50.0,
+                          child: Text('Text 3'),
+                        ),
+                        Container(
+                          width: double.infinity,
+                          color: Colors.green,
+                          height: 50.0,
+                          child: Text('Text 3'),
+                        ),
+                        Container(
+                          width: double.infinity,
+                          color: Colors.red,
+                          height: 50.0,
+                          child: Text('Text 3'),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-          // SliverToBoxAdapter(
-          //   child: Container(
-          //     color: Colors.pink,
-          //     height: 400.0,
-          //   ),
-          // ),
-          // SliverFillRemaining(),
-        ],
+            SliverToBoxAdapter(
+              child: Container(
+                color: Colors.pink,
+                height: 400.0,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
